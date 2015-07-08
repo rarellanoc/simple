a185bc98f5b1aa3e1c96f16f1c8cff232082fd5e
Ricardo Arellano <its.arellano@arqshow.com>
2015-07-08 08:54:31 -0400

    agregar tres paginas

--- simple.py
+++ simple.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask import render_template, url_for
 
@@ -7,6 +8,22 @@ app = Flask(__name__)
 def hola():
 	nombrevalue = "Ricardo"
 	return render_template('home.html', nombre=nombrevalue)
+
+@app.route("/coleccion")
+def holaColeccion():
+        nombrevalue = "Ricardo"
+        return render_template('coleccion.html', nombre=nombrevalue)
+
+@app.route("/directorio")
+def holaDirectorio():
+        nombrevalue = "Ricardo"
+        return render_template('directorio.html', nombre=nombrevalue)
+
+@app.route("/estadisticas")
+def holaEstadisticas():
+        nombrevalue = "Ricardo"
+        return render_template('estadisticas.html', nombre=nombrevalue)
+
 
 if __name__ == "__main__":
     app.debug = True
